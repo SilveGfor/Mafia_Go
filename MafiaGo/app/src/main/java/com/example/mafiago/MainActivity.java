e6b28af261dcac639cf00c479481a5e7ed4d9ee1
SilveGfor <63398117+SilveGfor@users.noreply.github.com>
2021-04-16 22:40:20 +0300

    computer

--- MafiaGo/app/src/main/java/com/example/mafiago/MainActivity.java
+++ MafiaGo/app/src/main/java/com/example/mafiago/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     public static String NickName = "";
     public static String Session_id = "";
     public static int Game_id;
-    public static String url = "ae60e8f8ba9d66.localhost.run";
+    public static String url = "f58098762b3e7d.localhost.run";
 
     public static String password = "";
     public static String nick = "";
@@ -28,6 +28,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getSupportFragmentManager().beginTransaction().replace(R.id.MainActivity, new GamesListFragment()).commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.MainActivity, new StartFragment()).commit();
     }
 }
