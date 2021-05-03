fda6431590b253f1b75cc748a040885bb7490f06
SilveGfor <63398117+SilveGfor@users.noreply.github.com>
2021-05-03 22:02:28 +0300

    computer

--- MafiaGo/app/src/main/java/com/example/mafiago/MainActivity.java
+++ MafiaGo/app/src/main/java/com/example/mafiago/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
     public static String NickName = "";
     public static String Session_id = "";
     public static int Game_id;
-    public static String url = "66167057bb8a68.localhost.run";
+    public static String url = "436e2e0981aaa1.localhost.run";
 
     public static String password = "";
     public static String nick = "";
